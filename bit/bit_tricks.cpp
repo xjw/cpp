2985288fc3b13439c7b65bcf9af081e7a829ccf5
oleole <oleole@gmail.com>
2010-12-23 13:31:31 -0800

    bit

--- bit/bit_tricks.cpp
+++ bit/bit_tricks.cpp
@@ -2,15 +2,40 @@
 
 using namespace std;
 
+/**
+ * returned number has x's lowest bit set to off 
+ */
+int getLowestBitOff (int x) {
+  return x & (x-1);
+}
+
+/**
+ * returned number is the number coming from the lowest set bit in x
+ * which means return is 2^n (n>=0)
+ */
+int getLowestSetBit (int x) {
+  return x ^ (x & (x-1));
+}
+
+int setithBit (int x, int i) {
+  return x | 1<<i;
+}
+
+int clearithBit (int x, int i) {
+  return x & ~(1<<i);
+}
+
+int toggleithBit (int x, int i) {
+  return x ^ (1<<i);
+}
+
+bool isithBitSet (int x, int i) {
+  return x & (1<<i);
+}
+
 int main() {
   int x;
   int i;
-  // set ith bit
-  x |= 1<<i;
-  // clear ith bit
-  x &= ~(1<<i);
-  // toggle ith bit
-  x ^= 1<<i;
-  // check
-  return x & (1<<i);
+
+  cin>>x;
 }
