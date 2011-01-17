3d635d8f4e917ff7ae66d669a43c3042d23ba9eb
xiangjun wang <xj@ubuntu.(none)>
2011-01-17 00:05:17 -0800

    all

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -4,7 +4,14 @@
 using namespace std;
 
 int main() {
-  graph<int> g(2);
-  g.addNode(1, 2, 10);
+  graph g(6);
+  g.addEdge(1, 2, 10);
+  g.addEdge(0, 1, 5);
+  g.addEdge(1, 3, 3);
+  g.addEdge(0, 4, 4);
+  g.addEdge(4, 5, 8);
+  g.BFS();
+  cout<<endl;
+  g.DFS();
   return 1;
 }
--- graph/graph.h
+++ graph/graph.h
@@ -2,40 +2,102 @@
 #define _XJ_GRAPH_
 
 #include <vector>
+#include <queue>
+#include <stack>
 #include <iostream>
+
 using namespace std;
 
-template <typename T>
-class edgeNode {
+class edge {
  public:
-  int idx;
-  T dist;
-  edgeNode(int, T);
+  int v;
+  int w;
+  edge(int vertice, int weight) : v(vertice), w(weight) {}
 };
 
-template <typename T>
-edgeNode<T>::edgeNode(int i, T d) {
-  idx = i;
-  dist = d;
+class node {
+ public:
+  int v;
+  vector<edge> edges;
+  void addEdge(int v, int weight);
+  node(int vertice) : v(vertice) {}
+};
+
+void node::addEdge(int v, int weight) {
+  edges.push_back(edge(v, weight));
 }
 
-template <typename T>
 class graph {
  public:
-  vector< vector<edgeNode<T> > > v;
+  vector<node *> v;
+  int VNUM;
   graph(int n);
-  void addNode(int, int, T);
+  ~graph();
+  void addEdge(int s, int d, int weight);
+  void BFS();
+  void DFS();
 };
 
-template <typename T>
-graph<T>::graph(int n) {
-  v.insert(v.begin(),NULL,n);
+graph::graph(int n) {
+  VNUM = n;
+  v.assign(n, NULL);
 }
 
-template <typename T>
-void graph<T>::addNode(int start, int end, T d) {
-  cout << v.size() << endl;
-  // v.at(start).push_back(edgeNode<T>(end, d));
+graph::~graph() {
+  for(vector<node *>::iterator it = v.begin(); it != v.end(); ++it) {
+    delete *it;
+  }
+}
+
+void graph::addEdge(int s, int d, int weight) {
+  if (v[s]==NULL) {
+    v[s] = new node(s);
+  }
+  if (v[d]==NULL) {
+    v[d] = new node(d);
+  }
+  v[s]->addEdge(d, weight);
+  v[d]->addEdge(s, weight);
+}
+
+void graph::BFS() {
+  bool *visited = new bool[VNUM];
+  for (int i=0; i<VNUM; ++i) {
+    visited[i] = false;
+  }
+
+  queue<int> q;
+  q.push(0);
+  while(!q.empty()) {
+    int front = q.front();
+    q.pop();
+    visited[front] = true;
+    cout << front << endl;
+    for (vector<edge>::iterator it=v[front]->edges.begin(); it != v[front]->edges.end(); ++it) {
+      int idx = (*it).v;
+      if (!visited[idx]) q.push(idx);
+    }
+  }
+
+  delete[] visited;
+}
+
+void graph::DFS() {
+  stack<int> st;
+  st.push(0);
+  bool *visited = new bool[VNUM];
+  for (int i=0; i<VNUM; ++i) visited[i] = false;
+
+  while(!st.empty()) {
+    int top = st.top();
+    cout << top << endl;
+    st.pop();
+    visited[top] = true;
+    for (vector<edge>::iterator it = v[top]->edges.begin(); it!= v[top]->edges.end(); ++it) {
+      int idx = (*it).v;
+      if (!visited[idx]) st.push(idx);
+    }
+  }
 }
 
 #endif
