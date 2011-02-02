37d435125d633bed795cefd471a262919603ad37
oleole <oleole@gmail.com>
2011-02-02 12:20:43 -0800

    path

--- tree/treeDiameter.cpp
+++ tree/treeDiameter.cpp
@@ -10,6 +10,7 @@
 
 using namespace std;
 
+// length only
 struct R {
     int depth;
     int length;
@@ -26,6 +27,49 @@ R diameter(BSTNode<int> *root) {
     return r;
 }
 
+///////////////////////////////////////////////////////////////
+
+// print path
+struct RPath {
+    int depth;
+    int length;
+    vector<int> v_d; // save depth path
+    vector<int> v_l; // save length path
+    RPath(int r=-1, int s=0) : depth(r), length(s) {}
+};
+
+RPath diameterPath(BSTNode<int> *root) {
+    RPath r;
+    if (!root) return r;
+    RPath left = diameterPath(root->left); 
+    RPath right = diameterPath(root->right);
+
+    if (left.depth>right.depth)
+        r.v_d = left.v_d;
+    else
+        r.v_d = right.v_d;
+
+    r.v_d.push_back(root->data);
+
+    int max_l = max(left.length, right.length);
+    if (2+left.depth+right.depth>max_l) {
+        r.v_l.insert(r.v_l.end(), left.v_d.begin(), left.v_d.end());
+        r.v_l.push_back(root->data);
+        r.v_l.insert(r.v_l.end(), right.v_d.rbegin(), right.v_d.rend());
+    }
+    else if (left.length>right.length) {
+        r.v_l = left.v_l;
+    }
+    else {
+        r.v_l = right.v_l;
+    }
+
+    r.depth = 1 + max(left.depth, right.depth);
+    r.length = max(2+left.depth+right.depth, max(left.length, right.length));
+    return r;
+}
+
+
 int main() {
     BST<int> *bst = new BST<int>;
     bst->insert_item(6);
@@ -40,6 +84,15 @@ int main() {
     R d = diameter(bst->root);
     cout << d.depth << endl;
     cout << d.length << endl;
+
+    cout<<endl;
+    RPath dpath = diameterPath(bst->root);
+    vector<int> v = dpath.v_l;
+    for(vector<int>::iterator it=v.begin(); it!=v.end(); ++it) {
+        cout<<*it<<endl;
+    }
+    cout<<endl;
+
 
     delete bst;
     return 1;
