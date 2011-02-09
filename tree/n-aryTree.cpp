2e712f4c93252c7fcdc2a51390d6274505cdcb41
oleole <oleole@gmail.com>
2011-02-08 17:28:44 -0800

    n-nary tree

---
+++ tree/n-aryTree.cpp
@@ -0,0 +1,26 @@
+/**
+ * http://hi.baidu.com/huifeng00/blog/item/63a4a4564db33d54d00906f7.html 
+ */
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+typedef int T;
+
+struct treeNode {
+    T data;
+    list<treeNode *> children;
+    treeNode(T d) : data(d) {}; 
+};
+
+treeNode* insert(treeNode *root, T d) {
+    treeNode *newnode = new treeNode(d);
+    root->children.push_back(newnode);
+    return newnode;
+}
+
+int main() {
+    return 1;
+}
