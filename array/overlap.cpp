d763bbb6625037ae651aad9e0d09dc7b132feb58
oleole <oleole@gmail.com>
2011-03-22 13:30:25 -0700

    more

---
+++ array/overlap.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits.h>
+
+using namespace std;
+
+typedef vector< pair<int, int> > V;
+
+struct comp {
+    bool operator () (pair<int, int> p1, pair<int, int> p2) {
+        return p1.first < p2.first;
+    }
+} mycomp;
+
+void merge_overlap(V v) {
+    int s1, e1, c;
+    int s2, e2;
+    V v2;
+    for (V::iterator it = v.begin(); it!=v.end(); it++) {
+        s2 = (*it).first;
+        e2 = (*it).second;
+        if (it == v.begin()) {
+            s1 = s2;
+            e1 = e2;
+            continue;
+        }
+        if (s2>e1) {
+            v2.push_back(make_pair(s1,e1));
+            s1 = s2;
+            e1 = e2;
+        }
+        else {
+            if (e2>e1) e1 = e2;
+        }
+    }
+
+    v2.push_back(make_pair(s1,e1));
+
+    for (V::iterator it = v2.begin(); it!= v2.end(); it++) {
+        cout<< (*it).first << "-" << (*it).second << endl;
+    }
+}
+
+int main() {
+    V v;
+    v.push_back(make_pair(1,3));
+    v.push_back(make_pair(8,10));
+    v.push_back(make_pair(2,6));
+    v.push_back(make_pair(15,18));
+    sort(v.begin(), v.end(), mycomp);
+
+    merge_overlap(v);
+    
+    return 1;
+}
--- cpp/copy_assignment_constructor.cpp
+++ cpp/copy_assignment_constructor.cpp
@@ -3,11 +3,57 @@
 using namespace std;
 
 class A {
+ public:
+  int c;
+  int *b;
+
+  A(void) {
+      cout<< "default constructor" << endl;
+  }
+  explicit A(int d) : c(d) {
+      cout<< "non-default constructor" << endl;
+  }
+
+  A(const A &rhs) {
+      cout<< "copy constructor" << endl;
+      c = rhs.c;
+  }
+
+/**
+ * http://en.wikipedia.org/wiki/Assignment_operator_(C%2B%2B) 
+ */
+  A& operator=(const A &rhs) {
+      if (this != &rhs) {
+          c = rhs.c;
+          if (b != NULL) {
+              delete [] b;
+              b = new int[rhs.c];
+              copy(rhs.b, rhs.b+rhs.c, b);
+          }
+      }
+      cout<< "copy assignment constructor" << endl;
+      return *this;
+  }
+
+  ~A() {
+      cout<< "default destructor" << endl;
+  }
 };
 
 class B : A {
+ public:
+    B() {
+        cout<< "default constructor from B" << endl;
+    }
+    ~B() {
+        cout<< "default destructor from B" << endl;
+    }
 };
 
 int main() {
+    A a = 3; 
+    // equivalent to A a( A(3) ); complier optimize this to be A(3)
+    // but copy constructor need to be accessible
+    // refer to http://stackoverflow.com/questions/2847787/constructor-or-assignment-operator
     return 1;
 }
