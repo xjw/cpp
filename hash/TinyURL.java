98eb7576492cd87cac82edfd5bdff31c2f09b2da
Xiangjun Wang <xwang@attinteractive.com>
2012-04-19 15:36:22 -0700

    TinyURL

---
+++ hash/TinyURL.java
@@ -0,0 +1,43 @@
+/*
+ * http://www.packtpub.com/article/url-shorteners-designing-tinyurl-clone-with-ruby
+ * The idea of designing tinyurl is fast, compact
+ * One quick idea is to use a centralized db to auto generate incremental ids 
+ * However ids are 10 based, not efficeint in terms of space
+ * We can convert 10 base to 62 base (26*2 <upper/lower case> + 10 = 62)
+ * e.g. 125 = 62^1 * 2 + 60^0 *1
+ *
+ */
+import java.util.*;
+
+public class TinyURL {
+    char[] table = new char[] {
+        '0','1','2','3','4','5','6','7','8','9',
+        'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+    };
+
+    public String getShortenedUrl(String url) {
+        return convert_10base_to_62base(getIdFromDB());
+    }
+
+    public String convert_10base_to_62base(long n) {
+        StringBuilder sb = new StringBuilder();
+        if (n == 0) sb.append(0);
+        while (n>0) {
+            sb.append(table[n%62]);
+            n = n/62;
+        }
+        System.out.println(n + "-" + sb);
+        return sb.toString();
+    }
+
+    public int getIdFromDB() {
+        Random r = new Random();
+        return r.nextInt(1000000);
+    }
+
+    public static void main(String[] args) {
+        TinyURL u = new TinyURL();
+        u.getShortenedUrl("htt://www.google.com");
+    }
+}
