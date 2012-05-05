d4435d34c02fb946b8dc952c79ba1eb168fcd218
Xiangjun Wang <xwang@attinteractive.com>
2012-05-05 15:32:56 -0700

    string misc

--- string/StringMisc.java
+++ string/StringMisc.java
@@ -46,6 +46,20 @@ public class StringMisc {
         return new String(a, 0, new_len);
     }
 
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        StringBuffer sb = new StringBuffer();
+        for (int i=0; i<strs[0].length(); i++) {
+            for (int j=1; j<strs.length; j++) {
+                if (i>=strs[j].length() || 
+                        strs[j].charAt(i) != strs[0].charAt(i))
+                    return sb.toString();
+            }
+            sb.append(strs[0].charAt(i));
+        }
+        return sb.toString();
+    add
+
     public static void main(String[] args) {
         StringMisc sm = new StringMisc();
         System.out.println(sm.removeCharInplace("llHello, world!ll",'l'));
