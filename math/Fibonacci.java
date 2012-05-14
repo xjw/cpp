ce4844fa45b05486718851e98edd2143fca5e596
Xiangjun Wang <xwang@attinteractive.com>
2012-05-13 22:35:37 -0700

    more

--- math/Fibonacci.java
+++ math/Fibonacci.java
@@ -1,11 +1,13 @@
+import java.util.*;
+
 public class Fibonacci {
-    public int fibRecursive(int n) {
+    static int fibRecursive(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
         return fibRecursive(n-1) + fibRecursive(n-2);
     }
 
-    public int fib(int n) {
+    static int fib(int n) {
         if (n == 0) return 0;
         int n1, n2;
         n1 = 0;
@@ -16,5 +18,25 @@ public class Fibonacci {
             n2 = t;
         }
         return n2;
+    }
+
+    static int fibTailRecursion(int n) {
+        return fibTailRecursion(n, 0, 1);
+    }
+
+    static int fibTailRecursion(int times, int n1, int n2) {
+        if (times == 0) return n1;
+        else if (times == 1) return n2;
+        return fibTailRecursion(times-1, n2, n1+n2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        while ((x=sc.nextInt())>=0) {
+            System.out.println(fib(x));
+            System.out.println(fibRecursive(x));
+            System.out.println(fibTailRecursion(x));
+        }
     }
 }
--- string/Strstr.java
+++ string/Strstr.java
@@ -1,19 +1,33 @@
 public class Strstr {
+    /*
+     * http://www.leetcode.com/2010/10/implement-strstr-to-find-substring-in.html
+     * General version strstr
+     */
     public String strStr(String haystack, String needle) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        if (needle == null || haystack == null 
-                || needle.length() > haystack.length()) {
-            return null;
-                } 
-
-        char[] h = haystack.toCharArray();
-        char[] n = needle.toCharArray();
-        int i, j;
-        for (i=0; i<=h.length-n.length; i++) {
-            for (j=0; j<n.length && h[i+j] == n[j]; j++);
-            if (j == n.length) return haystack.substring(i);
+        for (int i=0; i<=haystack.length()-needle.length(); i++) {
+            int j = 0;
+            while(j<needle.length()
+                    && haystack.charAt(i+j) == needle.charAt(j))
+                j++;
+            if (j == needle.length()) return haystack.substring(i);
         }
         return null;
+    }
+
+    /*
+     * haystack too big, stored in array
+     */
+    public String strStr(String[] haystack, String needle) {
+        int i, j, k;
+        i = j = 0;
+        for (int i=0; i<haystack.length; i++) {
+            String s = haystack[i];
+            k = 0;
+            while(j<needle.length && k<haystack.length 
+                    && s.charAt(k) == needle.charAt(j)) {
+                k++; j++;
+            }
+
+        }
     }
 }
