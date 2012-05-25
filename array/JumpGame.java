f723e0c831d05707a61bf0058239e55c54da4c1e
Xiangjun Wang <xwang@attinteractive.com>
2012-05-25 16:03:34 -0700

    jump game

--- array/JumpGame.java
+++ array/JumpGame.java
@@ -1,5 +1,17 @@
 import java.util.*;
 /*
+ * http://www.leetcode.com/onlinejudge
+ *
+ * JumpGame1
+ Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ Each element in the array represents your maximum jump length at that position.
+ Determine if you are able to reach the last index.
+ For example:
+ A = [2,3,1,1,4], return true.
+ A = [3,2,1,0,4], return false.
+
+ 
+ * JumpGame2
    Given an array of non-negative integers, you are initially positioned at the first index of the array.
    Each element in the array represents your maximum jump length at that position.
    Your goal is to reach the last index in the minimum number of jumps.
@@ -9,26 +21,68 @@ import java.util.*;
    (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
  */
 public class JumpGame {
+    // solution to problem 1:
+    // record the furthest step
+    public boolean canJump(int[] A) {
+        int max = 0;
+        int i=0;
+        while (i<=max) {
+            if (i+A[i]>max) max = i+A[i];
+            if (max>=A.length-1) return true;
+            i++;
+        }
+        return false;
+    }
+
+
+    /* solutions to problem 2
+     *
+     * Best Approach O(n)!!!! 
+     * Greedy, jump as far as possible
+     * http://www.mitbbs.com/article_t1/JobHunting/32118959_0_1.html
+     */
     public int jump(int[] A) {
+        int start, end, max, step;
+        start = end = step = 0;
+        while (end<A.length-1) {
+            max = 0;
+            while(start<=end) {
+                max = Math.max(start+A[start],max);
+                start++;
+            }
+            step++;
+            start = end+1;
+            end = max;
+        }
+        return step;
+    }
+
+    /*
+     * Approach 2: DP
+     */
+     public int jumpDP(int[] A) {
         int len = A.length;
         int[] cache = new int[len];
-
-        Arrays.fill(cache, len);
-        cache[len-1] = 0;
-
+        Arrays.fill(cache,len);
+        cache[len-1]=0;
+        
         for (int i=len-2; i>=0; i--) {
             int step = A[i];
-            if (step <= 0) {continue;}
-            if (i + step >= len -1) {
+            if (i+step>=len-1) {
                 cache[i] = 1;
             } else {
-                for (int j=i+1; j<=i+step; j++) {
-                    cache[i] = Math.min(cache[i], 1+cache[j]);
+                int min = len;
+                while(step>0) {
+                    min = Math.min(cache[i+step]+1, min);
+                    step--;
                 }
+                cache[i] = min;
             }
         }
         return cache[0];
     }
+
+    public 
 
     public static void main(String[] args) {
         JumpGame j = new JumpGame();
